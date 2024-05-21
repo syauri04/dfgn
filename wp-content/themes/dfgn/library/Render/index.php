e55ac0070c95e3e688252483c78e85175c09de01
Dzarr al ghifari <83385118+zarr20@users.noreply.github.com>
2024-05-21 19:17:28 +0700

    feat: add feature style per-component

---
+++ wp-content/themes/dfgn/library/Render/index.php
@@ -0,0 +1,21 @@
+<?php
+class Render {
+    private static $styles = '';
+
+    public static function Style() {
+        ob_start();
+    }
+
+    public static function EndStyle() {
+        self::$styles .= ob_get_clean();
+    }
+
+    public static function PrintStyles() {
+        if (!empty(self::$styles)) {
+            // Remove <style> tags from the output
+            $stylesWithoutTags = str_replace(['<style>', '</style>'], '', self::$styles);
+            echo $stylesWithoutTags;
+        }
+    }
+}
+?>
