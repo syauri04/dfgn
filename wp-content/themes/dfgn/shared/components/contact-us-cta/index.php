ea263da48dec3ab88df4e21e75a339e2584199b5
Dzarr al ghifari <83385118+zarr20@users.noreply.github.com>
2024-05-22 23:37:49 +0700

    fix(contact-us-component): fixing error

--- wp-content/themes/dfgn/shared/components/contact-us-cta/index.php
+++ wp-content/themes/dfgn/shared/components/contact-us-cta/index.php
@@ -1,12 +1,11 @@
 <?php
-if(!isset($param)){
-    $defaultParam = [
-        "title" => "Start or join a design factory!",
-        "button_text" => "Contact us",
-        "link" => "mailto:felipe.garate@aalto.fi"
-    ];
-    $param = array_merge($defaultParam, $param);
-}
+$param = [];
+$defaultParam = [
+    "title" => "Start or join a design factory!",
+    "button_text" => "Contact us",
+    "link" => "mailto:felipe.garate@aalto.fi"
+];
+$param = array_merge($param, $defaultParam);
 ?>
 
 <div class="container">
@@ -26,24 +25,27 @@ if(!isset($param)){
 
 <?php RenderStyle::Style(); ?>
 <style>
-@media (max-width: 991px) {
-    #contact-us-cta .row.border-head-cnt {
-        display: flex;
-        border-radius: 41px;
-        justify-content: center;
+    @media (max-width: 991px) {
+        #contact-us-cta .row.border-head-cnt {
+            display: flex;
+            border-radius: 41px;
+            justify-content: center;
+        }
+
+        #contact-us-cta .row.border-head-cnt .col-xl-12.d-flex.boxies {
+            flex-direction: column;
+            width: auto;
+        }
+
+        #contact-us-cta .row.border-head-cnt .col-xl-12.d-flex.boxies .txt-left {
+            text-align: center;
+            font-size: 24px;
+            margin-bottom: 25px;
+        }
+
+        #contact-us-cta .button {
+            padding: 20px
+        }
     }
-    #contact-us-cta .row.border-head-cnt .col-xl-12.d-flex.boxies {
-        flex-direction: column;
-        width: auto;
-    }
-    #contact-us-cta .row.border-head-cnt .col-xl-12.d-flex.boxies .txt-left {
-        text-align: center;
-        font-size: 24px;
-        margin-bottom: 25px;
-    }
-    #contact-us-cta .button {
-        padding:20px
-    }
-}
 </style>
 <?php RenderStyle::EndStyle(); ?>
