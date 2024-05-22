c77e783ab6c306455e07243d5ad371cc2a16266c
Dzarr al ghifari <83385118+zarr20@users.noreply.github.com>
2024-05-22 18:41:32 +0700

    feat: add minifier JShirnk

--- wp-content/themes/dfgn/library/Render/render-js.php
+++ wp-content/themes/dfgn/library/Render/render-js.php
@@ -1,4 +1,6 @@
 <?php
+
+require_once __DIR__ . '/../JShrink/Minifier.php';
 class RenderJS
 {
     private static $script = '';
@@ -17,7 +19,14 @@ class RenderJS
     {
         if (!empty(self::$script)) {
             $WithoutTags = str_replace(['<script>', '</script>'], '', self::$script);
-            echo $WithoutTags;
+            $minified = Minifier::minify($WithoutTags);
+            echo $minified;
         }
     }
+
+    // simpan ke file
+    // handle sesuai page yg generate
+    // tampilkan file cached sesuai page yang generate
+    // jika durasi cache habis generate kembali
+
 }
