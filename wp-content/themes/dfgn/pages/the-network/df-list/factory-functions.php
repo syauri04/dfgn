f176bb152fd63eaa53eb14c529ca91dcf64d8784
Dzarr al ghifari <83385118+zarr20@users.noreply.github.com>
2024-05-29 00:47:42 +0700

    update

--- wp-content/themes/dfgn/pages/the-network/df-list/factory-functions.php
+++ wp-content/themes/dfgn/pages/the-network/df-list/factory-functions.php
@@ -3,6 +3,8 @@ function createFactoryEntry($data)
 {
     ob_start();
     include 'factory-card.php';
+    ob_flush();
+    flush();
     $entry = ob_get_clean();
     return $entry;
 }
@@ -11,6 +13,8 @@ function createFactoryModal()
 {
     ob_start();
     include 'factory-card-modal.php';
+    ob_flush();
+    flush();
     $modal_content = ob_get_clean();
     return $modal_content;
 }
