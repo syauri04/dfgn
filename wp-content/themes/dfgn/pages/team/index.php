1482b952bde0b4ca8a35c6ff24b5a1fc724a2ece
Dzarr al ghifari <83385118+zarr20@users.noreply.github.com>
2024-05-29 00:52:33 +0700

    update

---
+++ wp-content/themes/dfgn/pages/team/index.php
@@ -0,0 +1,228 @@
+<!-- slider-area -->
+<!-- main-start -->
+<main>
+    <!-- slider-area -->
+    <section class="slider-scroll">
+        <div class="container">
+            <div class="main-slide-scroll team pt-50">
+                <div class="text-ss">
+                    <div class="main-text">
+                        <p>
+                            <?php the_field('title_hero'); ?>
+                        </p>
+                    </div>
+                    <div class="summary-text">
+                        <?php the_field('summary_hero'); ?>
+                    </div>
+
+                </div>
+                <div class="slide-frist">
+                    <div class="shape1 team" id="image1">
+
+                        <svg id="svg-main">
+                            <mask id="mask">
+                                <path d="M345.319 242.505C291.856 244.94 247.383 287.47 242.795 340.759C242.621 342.809 242.496 344.847 242.434 346.872C241.353 381.611 228.434 414.922 203.842 439.498L196.68 446.654C172.162 471.156 138.953 484.102 104.302 485.258C102.25 485.32 100.199 485.457 98.1226 485.631C44.8962 490.352 2.49914 534.795 0.111978 588.147C-2.80981 653.575 52.6048 706.877 118.762 700.478C167.748 695.732 208.044 657.302 214.882 608.598C215.467 604.448 215.815 600.323 215.927 596.26C216.934 561.409 229.839 527.974 254.507 503.323L261.084 496.751C285.689 472.162 319.047 459.241 353.835 458.147C355.862 458.085 357.901 457.961 359.952 457.787C413.291 453.19 455.849 408.722 458.249 355.271C461.109 291.719 408.939 239.597 345.331 242.492L345.319 242.505Z" fill="#D1D3D1" />
+                                <path d="M728.16 350.5V593.154C728.16 652.716 776.478 701 836.08 701C895.683 701 944 652.716 944 593.154V350.5C944 290.938 895.683 242.654 836.08 242.654C776.478 242.654 728.16 290.938 728.16 350.5Z" fill="#D1D3D1" />
+                                <path d="M728.136 98.3661C732.723 45.0767 777.197 2.54706 830.659 0.11182C894.267 -2.78313 946.437 49.3384 943.577 112.891C941.178 166.342 898.619 210.809 845.281 215.407C843.229 215.58 841.19 215.705 839.164 215.767C804.376 216.86 771.018 229.782 746.412 254.37L739.835 260.943C715.168 285.594 702.262 319.028 701.255 353.879C701.237 354.55 701.212 355.221 701.18 355.895V593.141C701.18 652.705 652.865 700.988 593.261 700.988H349.62C349.235 700.975 348.862 700.975 348.477 700.975C347.818 700.963 347.171 700.95 346.525 700.925C346.239 700.913 345.94 700.901 345.654 700.888C345.122 700.862 344.596 700.837 344.071 700.807C343.836 700.794 343.601 700.779 343.367 700.764C343.276 700.758 343.183 700.751 343.09 700.745C343.003 700.739 342.916 700.733 342.832 700.727L342.82 700.726C341.891 700.651 340.963 700.577 340.047 700.491H340.022C288.661 695.57 247.83 654.693 243.006 603.354C243.006 603.299 243 603.24 242.994 603.181C242.987 603.122 242.981 603.063 242.981 603.007C242.907 602.187 242.832 601.367 242.782 600.547L242.745 599.876C242.695 599.155 242.658 598.447 242.633 597.726C242.621 597.416 242.596 597.117 242.596 596.807C242.571 596.185 242.546 595.552 242.546 594.918C242.534 594.533 242.534 594.148 242.534 593.763V593.154C242.534 592.806 242.546 592.471 242.546 592.123C242.546 591.893 242.549 591.663 242.552 591.433C242.555 591.203 242.559 590.973 242.559 590.743C242.571 590.336 242.583 589.917 242.595 589.497L242.596 589.451C242.6 589.307 242.603 589.165 242.605 589.022C242.611 588.735 242.617 588.45 242.633 588.159L242.637 588.126C242.648 588.027 242.658 587.938 242.658 587.849C242.72 586.507 242.82 585.177 242.932 583.86C242.95 583.663 242.969 583.462 242.987 583.262C243.006 583.058 243.025 582.855 243.043 582.655C243.18 581.326 243.329 580.009 243.503 578.704C243.541 578.406 243.59 578.095 243.628 577.797C243.827 576.331 244.063 574.865 244.337 573.423V573.374C250.578 539.728 272.435 511.573 302.113 496.701C313.9 490.725 326.905 486.836 340.644 485.618C342.72 485.444 344.772 485.308 346.823 485.246C381.474 484.09 414.683 471.144 439.201 446.642L446.363 439.486C470.956 414.91 483.874 381.599 484.955 346.86C485.018 344.834 485.142 342.797 485.316 340.747C489.904 287.457 534.377 244.928 587.84 242.492C590.881 242.354 593.896 242.342 596.88 242.45C628.89 239.845 659.215 227.052 682.021 204.262L689.182 197.105C713.775 172.529 726.693 139.218 727.775 104.479C727.837 102.454 727.962 100.416 728.136 98.3661Z" fill="#D1D3D1" />
+
+                            </mask>
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" id="pink-bulet" width="217" height="216" viewBox="0 0 217 216" fill="none">
+                            <!-- <g style="mix-blend-mode:color"> -->
+                            <path d="M108.261 215.692C167.863 215.692 216.18 167.408 216.18 107.846C216.18 48.2844 167.863 0 108.261 0C48.6582 0 0.34082 48.2844 0.34082 107.846C0.34082 167.408 48.6582 215.692 108.261 215.692Z" fill="#FA5448" />
+                            <!-- </g> -->
+                        </svg>
+                        <div class="main-image-team">
+                            <img src="<?php the_field('image_hero'); ?>" alt="Balloons">
+                        </div>
+                        <svg xmlns="http://www.w3.org/2000/svg" id="kuning-bulet" width="216" height="216" viewBox="0 0 216 216" fill="none">
+                            <path d="M107.92 215.692C167.522 215.692 215.84 167.408 215.84 107.846C215.84 48.2844 167.522 0 107.92 0C48.3174 0 0 48.2844 0 107.846C0 167.408 48.3174 215.692 107.92 215.692Z" fill="#FEC601" />
+                        </svg>
+                    </div>
+
+
+                </div>
+
+            </div>
+        </div>
+
+    </section>
+    <section class="service-area">
+        <div class="container-history pteam">
+            <div class="r-team">
+                <div class="part-history">
+                    <div class="imageh" style="background: linear-gradient(0deg, rgba(24, 34, 36, 0.4) 0%, rgba(24, 34, 36, 0.4) 100%), url(<?php the_field('background_image'); ?>) lightgray 50% / cover no-repeat;">
+                        <div class="overly">
+
+                        </div>
+                        <div class="txt-history">
+                            <div class="title">
+                                <?php the_field('title_history'); ?>
+                            </div>
+                            <div class="summary">
+                                <?php the_field('desc_history'); ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+
+
+    <section class="section-area pt-120">
+        <div class="container mb-30">
+            <div class="row border-head-team">
+                <div class="col-xl-12 js-center">
+                    <div class="col-xl-5 col-md-5 pl-0">
+                        <h1><?php the_field('title_breadcumb'); ?></h1>
+
+                    </div>
+
+                    <div class="col-xl-5 col-md-5">
+                        <p>
+                            <?php the_field('summary_breadcumb'); ?>
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="row pt-30">
+                <?php
+                if (have_rows('who_runs')) :
+                    while (have_rows('who_runs')) : the_row();
+                ?>
+                        <div class="col-xl-6 col-md-6">
+                            <div class="boxies-team">
+
+                                <div class="img-team">
+                                    <div class="gradient-overlay"></div>
+
+                                    <img class="pro" src="<?php echo get_sub_field('picture'); ?>" alt="">
+                                    <div class="user-in">
+                                        <div class="nm">
+                                            <?php echo get_sub_field('name_profile'); ?>
+                                        </div>
+                                        <div>
+                                            <a href="mailto:<?php echo get_sub_field('email'); ?>"><img src="<?php echo get_template_directory_uri() . '/assets/img/assets/df/icw_email.png'; ?>" alt=""></a>
+                                            <a href="<?php echo get_sub_field('link_linkedin'); ?>" target="_blank"><img src="<?php echo get_template_directory_uri() . '/assets/img/assets/df/icw_linkedin.png'; ?>" alt=""></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="info-team">
+                                    <p>
+                                        <?php echo get_sub_field('desc_profile'); ?>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                <?php
+                    endwhile;
+                endif;
+                ?>
+
+
+
+
+
+            </div>
+        </div>
+    </section>
+
+
+
+</main>
+
+<script>
+    $(document).ready(function() {
+        $('#select-primer').awselect({
+            background: "#182224", //the dark blue background
+            active_background: "#182224", // the light blue background
+            placeholder_color: "#FFFFFF", // the light blue placeholder color
+            placeholder_active_color: "#FFFFFF", // the dark blue placeholder color
+            option_color: "#FFFFFF", // the option colors
+
+            horizontal_padding: "40px", // left and right padding,
+            immersive: false // immersive option, demonstrated at the next example
+        });
+    });
+</script>
+<script>
+    // TweenMax for GSAP 3
+    var tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".container-history",
+            scrub: 1,
+            pin: true,
+            // markers: true,
+            start: "top top",
+            end: "+=100%",
+            // toggleActions: "play pause play reset",
+        },
+    });
+    tl.to(".r-team", {
+        width: "100%",
+
+        height: "800",
+        borderRadius: "0",
+        duration: 1,
+        ease: "Power0.easeNone",
+
+    });
+
+    var tl2 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".container-history",
+            scrub: 1,
+            // markers: true,
+            start: "top top",
+            end: "+=100%",
+            // toggleActions: "play pause play reset",
+        },
+    });
+    tl2.to(".imageh", {
+        // background: "linear-gradient(0deg, rgba(24, 34, 36, 0.2) 0%, rgba(24, 34, 36, 0.2) 100%), , url(img/assets/team-background.png) lightgray 50% / cover no-repeat",
+        opacity: 0.2,
+        width: "55%",
+        height: "799",
+        borderRadius: "0",
+        opacity: 1,
+        border: "0",
+        duration: 1,
+        ease: "Power0.easeNone",
+
+    });
+
+    gsap.set(".txt-history", {
+
+        right: "130px",
+    });
+    var tl3 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".container-history",
+            scrub: 1,
+            // markers: true,
+            start: "top top",
+            end: "+=100%",
+            // toggleActions: "play pause play reset",
+        },
+    });
+    tl3.to(".txt-history", {
+        right: "200px",
+        duration: 1,
+        ease: "Power0.easeNone",
+
+    });
+    // gsap.to('.imageh', {
+    //     backgroundSize: '150%', // Change background size to cover full screen
+    //     ease: 'none',
+    //     scrollTrigger: {
+    //         trigger: '.pteam',
+    //         start: 'top top', // Trigger animation when top of container reaches top of viewport
+    //         end: '+=100%', // Extend animation beyond container height
+    //         scrub: 1 // Smooth animation based on scroll
+    //     }
+    // });
+</script>
